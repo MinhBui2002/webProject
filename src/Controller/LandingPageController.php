811f30ea5e545501df3dab3ff15ca990936826f2
MinhBui2002 <88824973+MinhBui2002@users.noreply.github.com>
2021-12-28 23:03:18 +0700

    Add files via upload

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -1,16 +1,31 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Order;
+use App\Entity\OrderDetail;
 use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
+use Symfony\Component\Security\Core\Security;
+use Exception;
+use App\Doctrine\Persistence\ObjectManager;
+use App\Form\OrderCustomerType;
+use App\Form\OrderType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class LandingPageController extends AbstractController
 {
-    
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
     /**
      * @Route("/customer/landingPage/", name="landing_page_category")
      */
@@ -42,16 +57,48 @@ class LandingPageController extends AbstractController
     /**
      * @Route("/customer/viewProduct/{id}", name = "landing_page_view_product")
      */
-    public function viewProduct ($id) {
+    public function viewProduct ($id, Request $request) {
+
         $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if ($product != null) {
+        $order = new Order;
+       
+        $form = $this->createForm(OrderCustomerType::class,
+        // [
+        //    'OrderDateVariable' => $createDate,
+        //     'UserNameVariable' => $user,
+        //     'ProductPrice' => $productPrice,
+        //     'TotalAmount'=> $total,
+        //     'ProductVariable' =>$productID
+        //  ]
+    );
+        
+    if ($product != null) {
             return $this->render("customer/productDetail.html.twig",
             [
-                'product' => $product
+                'product' => $product,
+                'form' =>$form->createView()
             ]
             );
         } else {
             return $this->redirectToRoute("landing_page_view_category");
-        }     
+        }    
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $order->setOrderDate(new \DateTime());
+
+            $order->setUser($this->security->getUser()->getId());
+
+            
+            $order->getOrderDetail()->addProduct($product[$id]);
+            $order->getOrderDetail()->setDetailTotal($product->getProductPrice()*$form->getData('DetailQuantity'));
+            
+            $manager->persist($order);
+            $manager->flush();
+            return $this->redirectToRoute("view_all_order");
+        }
     }
+
+    
 }
+
