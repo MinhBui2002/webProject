e809735be22b4928f38335efc86ae35b9d77c3b7
MinhBui2002 <88824973+MinhBui2002@users.noreply.github.com>
2021-12-29 16:31:23 +0700

    Add files via upload

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,21 +2,41 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
+
+use App\Entity\Order;
 use App\Entity\Product;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\OrderType;
+use App\Entity\Category;
+use App\Entity\OrderDetail;
+use App\Form\OrderCustomerType;
+use App\Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
 
 class LandingPageController extends AbstractController
 {
-    
+    public function __construct(SessionInterface $session, Security $security)
+    {
+        $this->security = $security;
+        $this->session = $session;
+    }
     /**
      * @Route("/customer/landingPage/", name="landing_page_category")
      */
-    public function viewAllCategory () {
+    public function viewAllCategory()
+    {
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
-        return $this->render("customer/index.html.twig",
+        return $this->render(
+            "customer/index.html.twig",
             [
                 'categories' => $categories
             ]
@@ -26,32 +46,49 @@ class LandingPageController extends AbstractController
     /**
      * @Route("/customer/viewCategory/{id}", name = "landing_page_view_category")
      */
-    public function viewCategory ($id) {
+    public function viewCategory($id)
+    {
         $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
         if ($category != null) {
-            return $this->render("customer/categoryDetail.html.twig",
-            [
-                'category' => $category
-            ]
+            return $this->render(
+                "customer/categoryDetail.html.twig",
+                [
+                    'category' => $category
+                ]
             );
         } else {
             return $this->redirectToRoute("landing_page_category");
-        }     
+        }
     }
 
     /**
      * @Route("/customer/viewProduct/{id}", name = "landing_page_view_product")
      */
-    public function viewProduct ($id) {
+    public function viewProduct($id, Request $request)
+    {
+        $order = new order;
+        $form = $this->createForm(OrderCustomerType::class, $order);
+        $form->handleRequest($request);
+        $orderDetail = $order->getOrderDetail();
         $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if ($product != null) {
-            return $this->render("customer/productDetail.html.twig",
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order->setUser($this->security->getUser());
+            $order->setOrderDate(new \DateTime());
+            $var = $form["orderDetail"]["DetailQuantity"]->getData();
+            $orderDetail->setDetailPrice($product->getProductPrice());
+            $orderDetail->setDetailTotal($product->getProductPrice() * $var);
+            $orderDetail->addProduct($product);
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($order, $orderDetail);
+            $manager->flush();
+            return $this->redirectToRoute("landing_page_view_product",array('id'=>$id));
+        }
+        return $this->render(
+            "customer/productDetail.html.twig",
             [
-                'product' => $product
+                'product' => $product,
+                'form' => $form->createView()
             ]
-            );
-        } else {
-            return $this->redirectToRoute("landing_page_view_category");
-        }     
+        );
     }
 }
