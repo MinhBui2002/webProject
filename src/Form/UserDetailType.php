3fd89486f61b5bc40d60994c91c78e91954c03e3
buivuthang <95832695+buivuthang@users.noreply.github.com>
2021-12-29 22:24:09 +0700

    Register CSS

--- src/Form/UserDetailType.php
+++ src/Form/UserDetailType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\UserDetail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class UserDetailType extends AbstractType
