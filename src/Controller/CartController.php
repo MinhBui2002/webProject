c64f6baa7f439411fa2c4d9fdd6407175f614e33
MinhBui2002 <88824973+MinhBui2002@users.noreply.github.com>
2021-12-29 16:24:05 +0700

    Add files via upload

---
+++ src/Controller/CartController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Security;
+
+class CartController extends AbstractController
+{
+
+    public function __construct(SessionInterface $session, Security $security)
+    {
+        $this->security = $security;
+        $this->session = $session;
+    }
+    /**
+     * @Route("/cart/viewAllOrder", name="view_cart")
+     */
+    public function index(): Response
+    {
+        $orderID = $this->security->getUser();
+        // $orders = $this->getDoctrine()->getRepository(Order::class)->find($orderID);
+        $orders = $this->getDoctrine()->getRepository(Order::class)->findAll();
+        return $this->render('cart/index.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name = "cart_delete_order_by_id")
+     */
+    public function deleteOrder ($id) {
+        $order = $this->getDoctrine()->getRepository(Order::class)->find($id);
+        if ($order != null) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($order);
+            $manager->flush();
+        }
+        return $this->redirectToRoute("view_cart");
+    }
+
+}
